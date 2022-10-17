b4d956bfcfa6fe53dedfebd25902f522617fc444
sthAneal <anilmeronaam@gmail.com>
2022-10-17 19:28:08 +1100

    re-configurating git origin

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,8 @@ import { ViewEvents } from "./components/ViewEvents";
 
 import { DashboardProvider } from "./context/DashboardContext";
 
-// import { Login } from './components/Login';
-// import { Register } from './components/Register';
-// import { Woolie } from './components/Woolie';
-// import { Coles } from './components/Coles';
-
 function App() {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" caseSensitive={false} element={<Layout/>}>
-    //       <Route index element={<HomeContextProvider><Home/></HomeContextProvider>}/>
-    //       <Route path="/mynote" caseSensitive={false} element={<MyNotesProvider><MyNoteWrapper/></MyNotesProvider>}>
-    //         <Route path="/mynote" caseSensitive={false} element={<Auth/>}/>
-    //         <Route path="/mynote/docket" caseSensitive={false} element={<MyNote/>}>
-    //         </Route>
-    //       </Route>
-    //       <Route path="*" element={<Error/>}/>
-    //     </Route>
-    //   </Routes>
-    // </Router>
-
     <Router>
       <Routes>
         <Route path="/" caseSensitive={false} element={<Layout/>}>
